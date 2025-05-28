6c0ad12ab36ef27850501f606e36b4f131b0b4f6
Sorif Ahmed <sharifahmed1413@gmail.com>
2025-05-28 14:43:51 +0600

    ecommerce project

---
+++ lib/features/home/data/models/slider_model.dart
@@ -0,0 +1,16 @@
+class SliderModels{
+  final String id;
+  final String photoUrl;
+  final String description;
+  final String? brand;
+  final String? category;
+
+  SliderModels({
+    required this.id,
+    required this.photoUrl,
+    required this.description,
+    required this.brand,
+    required this.category
+  });
+  SliderModels.f
+}
